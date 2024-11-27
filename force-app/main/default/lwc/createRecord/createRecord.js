f19a5b66eda5a207b7bb1804760cf4a651248694
Sreshta Gollapally <sreshtag11@gmail.com>
2024-11-27 21:07:47 +0530

    js changes

--- force-app/main/default/lwc/createRecord/createRecord.js
+++ force-app/main/default/lwc/createRecord/createRecord.js
@@ -1,11 +1,13 @@
 import { LightningElement } from 'lwc';
-import getoperation from '@salesforce/apex/createJob.operation'
-
+import createJob from '@salesforce/apex/createJob.createJob'
 export default class CreateRecord extends LightningElement {
   col
+  JobId
   dataInTable
   columns
   value = '';
+  operation
+  sendString=""
   get operations() {
     return [
       { label: 'Insert', value: 'Insert' },
@@ -21,23 +23,26 @@ export default class CreateRecord extends LightningElement {
       { label: 'three', value: '3' },
     ];
   }
-  handleOperation(){
-    
+  handleOperation(event){
+    this.operation=event.target.value
   }
   handleOptionChange(){
 
   }
+
   changeHandler(event) {
     const coloumns = []
     const data = []
     const arr = event.target.value.split('\n');
     this.col = arr[0].split("\t")
+    this.sendString+=this.col.join(",")+"\r\n"
     this.col.forEach(element => {
       coloumns.push({ label: element, fieldName: element, type: 'text' })
     });
     for (let i = 1; i < arr.length; i++) {
       const tableData = {}
       const linedata = arr[i].split("\t")
+      this.sendString+=linedata.join(",")+"\r\n"
       if (coloumns.length == linedata.length) {
         for (let j = 0; j < coloumns.length; j++) {
           tableData[coloumns[j].fieldName] = linedata[j]
@@ -47,13 +52,18 @@ export default class CreateRecord extends LightningElement {
     }
     this.dataInTable = data
     this.columns = coloumns
-    console.log("This is columns data", JSON.stringify(coloumns))
-    console.log("This is Data in table", JSON.stringify(data))
   }
+  uploadData(){
 
+  }
   handleOnclick(){
-    
-    
-  }
+    createJob().then(result=>{
+      this.JobId=result.Id
+      console.log("Job Id",result.Id)
 
+    }).catch(error=>{
+      console.log(error)
+      console.log("some pichakuntla error")
+    })
+  }
 }
