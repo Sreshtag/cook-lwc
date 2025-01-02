071b2e4c4c65a861631c9dbb9f626269846bb7a4
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-02 21:50:46 +0530

    inline editing example

--- force-app/main/default/lwc/createRecord/createRecord.js
+++ force-app/main/default/lwc/createRecord/createRecord.js
@@ -1,41 +1,68 @@
-import { LightningElement } from 'lwc';
+import { LightningElement} from 'lwc';
 import createJob from '@salesforce/apex/createJob.createJob'
+import getObjects from '@salesforce/apex/createJob.getObjects'
+
 export default class CreateRecord extends LightningElement {
-  requestState =true
-  ResponseState=false
+  requestState = true
+  ResponseState = false
   col
-  errortem=false
-  JobId=''
-  upsert=false
+  errortem = false
+  JobId = ''
+  upsert = false
   dataInTable
   columns
-  sendString=""
-  operationInfo = {"operation" : 'insert',"object" : 'Account',"contentType" : "CSV","lineEnding" : "CRLF"}
-  get operations() {
-    return [
-      { label: 'Insert', value: 'insert' },
-      { label: 'Update', value: 'update' },
-      { label: 'Upsert', value: 'upsert' },
-      { label: 'Delete', value: 'delete' },
-    ];
+  sendString = ""
+  operationInfo = { "operation": 'insert', "object": 'Account', "contentType": "CSV", "lineEnding": "CRLF" }
+  customSettings=[]
+  objects = []
+  connectedCallback() {
+    getObjects().then(result => {
+      const objRecs = JSON.parse(result).records
+      objRecs.forEach(object => {
+        if (object.IsCustomSetting) {
+          this.customSettings.push(object.QualifiedApiName)
+        }
+        else {
+          this.objects.push({ label: object.QualifiedApiName, value: object.QualifiedApiName })
+        }
+      });
+    }).catch(error => {
+      console.log(error)
+    })
   }
-  get objects() {
-    return [
-      { label: 'Account', value: 'Account' },
-      { label: 'Contact', value: 'Contact' },
-      { label: 'Opportunity', value: 'Opportunity' },
-    ];
+   get operations() {
+     return [
+       { label: 'Insert', value: 'insert' },
+       { label: 'Update', value: 'update' },
+       { label: 'Upsert', value: 'upsert' },
+       { label: 'Delete', value: 'delete' },
+     ];
+   }
+
+  get customSettings(){
+
   }
-  handleOperation(event){
-    event.target.value === "update"? this.update = true : this.update = false
+
+  // get objects() {
+  //   return [
+  //     { label: 'Account', value: 'Account' },
+  //     { label: 'Contact', value: 'Contact' },
+  //     { label: 'Opportunity', value: 'Opportunity' },
+  //   ];
+  // }
+
+  handleOperation(event) {
+    console.log(JSON.stringify(this.objectInfo))
+    event.target.value === "update" ? this.update = true : this.update = false
     this.operationInfo.operation = event.target.value
-    console.log(JSON.stringify(this.operationInfo))
+    //console.log(JSON.stringify(this.operationInfo))
   }
-  handleOptionChange(event){
+  handleOptionChange(event) {
     this.operationInfo.object = event.target.value
-    console.log(JSON.stringify(this.operationInfo))
+    //console.log(JSON.stringify(this.operationInfo))
   }
-  handleupsert(event){
+  
+  handleupsert(event) {
     this.operationInfo.externalIdFieldName = event.target.value
   }
 
@@ -44,14 +71,14 @@ export default class CreateRecord extends LightningElement {
     const data = []
     const arr = event.target.value.split('\n');
     this.col = arr[0].split("\t")
-    this.sendString+=this.col.join(",")+"\r\n"
+    this.sendString += this.col.join(",") + "\r\n"
     this.col.forEach(element => {
       coloumns.push({ label: element, fieldName: element, type: 'text' })
     });
     for (let i = 1; i < arr.length; i++) {
       const tableData = {}
       const linedata = arr[i].split("\t")
-      this.sendString+=linedata.join(",")+"\r\n"
+      this.sendString += linedata.join(",") + "\r\n"
       if (coloumns.length == linedata.length) {
         for (let j = 0; j < coloumns.length; j++) {
           tableData[coloumns[j].fieldName] = linedata[j]
@@ -62,15 +89,13 @@ export default class CreateRecord extends LightningElement {
     this.dataInTable = data
     this.columns = coloumns
   }
-  handleOnclick(){
-    console.log(this.sendString)
-    createJob({Body : this.sendString , operationInfo : JSON.stringify(this.operationInfo)}).then(result=>{
-      console.log("Job Id",result)
-      this.requestState=false
-      this.ResponseState=true
-      this.JobId=result
-    }).catch(error=>{
-      this.errortem=true
+  handleOnclick() {
+    createJob({ Body: this.sendString, operationInfo: JSON.stringify(this.operationInfo) }).then(result => {
+      this.requestState = false
+      this.ResponseState = true
+      this.JobId = result
+    }).catch(error => {
+      this.errortem = true
       console.log(error)
     })
   }
